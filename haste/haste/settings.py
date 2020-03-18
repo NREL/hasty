9ad6b4937d58423237c3a6dfdfefae22f7e7c2e2
aviveiros11 <viveiros.austin@gmail.com>
2020-03-18 13:39:50 -0600

    Modified settings

--- haste/haste/settings.py
+++ haste/haste/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'haste.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ["haste/templates/"],
+        'DIRS': ["haste/haste/templates/"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
