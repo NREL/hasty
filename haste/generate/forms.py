3848e5e5676c105d72a3f65d19ad9617ad0d318c
corymosiman12 <cory.mosiman12@gmail.com>
2020-06-04 15:28:08 -0600

    update models and forms

--- haste/generate/forms.py
+++ haste/generate/forms.py
@@ -6,3 +6,22 @@ class SiteForm(forms.ModelForm):
     class Meta:
         model = models.Site
         fields = ('name', 'city', 'state',)
+
+class AirSystemsForm(forms.ModelForm):
+    class Meta:
+        model = models.AirSystems
+        fields = ("name",)
+
+
+class AirHandlerForm(forms.ModelForm):
+    class Meta:
+        model = models.AirHandler
+        fields = ("name", "heating_coil_type", "cooling_coil_type")
+
+
+# class TerminalUnitForm(forms.ModelForm):
+
+
+    # def __init__(self, site_id, *args, **kwargs):
+    #     super(AirSystemsForm, self).__init__(*args, **kwargs)
+    #     self.fields['site_id'].queryset = models.Site.objects.filter(id=site_id)
---
+++ haste/generate/utils.py
@@ -0,0 +1,26 @@
+
+HEATING_COILS = [
+  {
+    "id": 1,
+    "description": "Heating Coil 1",
+    "tags": ["tag1", "tag2"]
+  },
+  {
+    "id": 2,
+    "description": "Heating Coil 2",
+    "tags": ["tag3", "tag4"]
+  }
+]
+
+COOLING_COILS = [
+  {
+    "id": 1,
+    "description": "Heating Coil 1",
+    "tags": ["tag1", "tag2"]
+  },
+  {
+    "id": 2,
+    "description": "Heating Coil 2",
+    "tags": ["tag1", "tag2"]
+  }
+]
