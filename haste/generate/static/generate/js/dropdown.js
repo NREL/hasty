d088174f32e4f33de4f8e733a6d05c5e8bb5c89c
aviveiros11 <viveiros.austin@gmail.com>
2020-06-18 15:11:38 -0600

    adding jstree

--- haste/generate/static/generate/js/dropdown.js
+++ haste/generate/static/generate/js/dropdown.js
@@ -1,10 +1,14 @@
-var toggler = document.getElementsByClassName("caret");
-var i;
-
-for (i = 0; i < toggler.length; i++) {
-    toggler[i].addEventListener("click", function () {
-        this.parentElement.querySelector(".nested").classList.toggle("active");
-        this.classList.toggle("caret-down");
+$(function () {
+    $('#jstree').jstree({
+        "core": {
+            "animation": 0,
+            "check_callback": true,
+            "themes": {
+                "stripes": false,
+                "dots": false,
+                "icons": false
+            },
+        },
     });
-}
+});
 
