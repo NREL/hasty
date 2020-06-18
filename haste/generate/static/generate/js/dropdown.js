8875bcf825676b4df9590a38068df75b42a1c9ff
aviveiros11 <viveiros.austin@gmail.com>
2020-06-18 15:33:44 -0600

    better formatting

--- haste/generate/static/generate/js/dropdown.js
+++ haste/generate/static/generate/js/dropdown.js
@@ -1,5 +1,19 @@
 $(function () {
-    $('#jstree').jstree({
+    $('#point_jstree').jstree({
+        "core": {
+            "animation": 0,
+            "check_callback": true,
+            "themes": {
+                "stripes": false,
+                "dots": false,
+                "icons": false
+            },
+        },
+    });
+});
+
+$(function () {
+    $('#equip_jstree').jstree({
         "core": {
             "animation": 0,
             "check_callback": true,
