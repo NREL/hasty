513abf46502f132c1ee94a303b64cb4e86806620
corymosiman12 <cory.mosiman12@gmail.com>
2020-08-12 20:36:09 -0600

    create simple yaml structure for equipment templates

--- haste/templater/models.py
+++ haste/templater/models.py
@@ -7,5 +7,9 @@ from mapp.models import HaystackVersion, HaystackPointType, HaystackEquipmentTyp
 class HaystackEquipmentTemplate(models.Model):
     name = models.CharField(max_length=100, null=False, blank=False)
     version = models.ForeignKey(HaystackVersion, on_delete=models.CASCADE)
+    description = models.TextField(max_length=1000, null=True, blank=True)
     equipment_type = models.ForeignKey(HaystackEquipmentType, on_delete=models.CASCADE)
     points = models.ManyToManyField(HaystackPointType)
+
+# For trying to get uniqueness across version, equipment_type, and points:
+# https://stackoverflow.com/questions/55196633/unique-together-and-m2m-field
