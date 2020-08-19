3f1f8ac8c67d97eeeb46beafdc1d2977e7a96d0a
corymosiman12 <cory.mosiman12@gmail.com>
2020-08-18 23:04:09 -0600

    fix travis

--- haste/haste/settings.py
+++ haste/haste/settings.py
@@ -89,7 +89,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'TEST_NAME': os.path.join(BASE_DIR, 'test.db')
+        'TEST': {
+            'NAME': os.path.join(BASE_DIR, 'test.db')
+        }
     }
 }
 
