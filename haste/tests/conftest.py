e833e5a54b708039d5094e3ccf6935f0e1850bd1
corymosiman12 <cory.mosiman12@gmail.com>
2020-08-20 11:00:44 -0600

    test override setup

--- haste/tests/conftest.py
+++ haste/tests/conftest.py
@@ -1,5 +1,16 @@
-
+from django.conf import settings
+import pytest
 
 # @pytest.fixture(autouse=True)
 # def enable_db_access_for_all_tests(db):
 #     pass
+
+
+@pytest.fixture(scope='session')
+def django_db_modify_db_settings(request):
+    settings.DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': 'test.db'
+        }
+    }
