096f1a5d34061832bba33c601cfa945b283eaa5e
aviveiros11 <viveiros.austin@gmail.com>
2020-08-20 17:32:23 -0400

    name change

--- haste/generate/urls.py
+++ haste/generate/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.ListSites.as_view(), name='index'),
     path('create_site/', views.CreateSite.as_view(), name='create-site'),
-    path('create_from_template/', views.CreateFromTemplate.as_view(), name='create-from_template'),
+    path('create_from_template/', views.CreateFromTemplate.as_view(), name='create-from-template'),
     path('site/<site_id>', views.SiteDetail.as_view(), name='site-detail'),
     path('site/<site_id>/ahu/<ahu_id>', views.AirHandler.as_view(), name='site.ahu'),
     path('data_view/<site_id>', views.data_view, name='data_view')
