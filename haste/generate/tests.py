1541dc9e6f481454d452f448e621f84740387b1b
aviveiros11 <viveiros.austin@gmail.com>
2020-08-21 11:46:29 -0400

    adding POST test for delete

--- haste/generate/tests.py
+++ haste/generate/tests.py
@@ -6,7 +6,6 @@ from . import models
 class IndexViewTest(TestCase):
 
     def setUp(self):
-
         test_site = models.Site.objects.create(
             id=1111,
             name="Test Site",
@@ -29,3 +28,13 @@ class IndexViewTest(TestCase):
         self.assertTrue('ahus' in response.context)
         self.assertEqual([site.name for site in response.context['sites']], ["Test Site"])
         self.assertEqual([ahu.name for ahu in response.context['ahus']], ["Test AHU"])
+
+    def test_delete(self):
+        # Get the test_site for the UUID
+        response = self.client.get('')
+        site = response.context['sites'][0]
+        # Send the delete in a POST
+        response = self.client.post('', {'id': site.id, 'delete': ""})
+        # The site GET request should now be 0
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(list(response.context['sites']), [])
