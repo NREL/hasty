ac24f7cb5f0dbcc72e171fa9379b0661d27f75cd
aviveiros11 <viveiros.austin@gmail.com>
2020-08-25 11:33:27 -0400

    fixed duplicated filter problem

--- haste/mapp/views.py
+++ haste/mapp/views.py
@@ -2,7 +2,7 @@
 from django.shortcuts import render
 from django.views.generic import CreateView
 
-from .models import HaystackPointType, BrickPointType, PointTypeMap
+from .models import BrickPointType, HaystackPointType
 from .filters import BrickPointTypeFilter, HaystackPointTypeFilter, PointTypeMapFilter
 
 
@@ -19,14 +19,17 @@ class PointMappingView(CreateView):
             }
             return render(request, self.template_name, args)
         else:
-            ptm = PointTypeMap.objects.all()
-            map_filter = PointTypeMapFilter(request.GET)
 
-            data = request.GET.copy()
-            data['inference_version'] = '2'
-            map_filter1 = PointTypeMapFilter(data)
-            ptm1 = map_filter1.qs
-            ptm = map_filter.qs
+            inf_1_data = request.GET.copy()
+            inf_1_data['inference_version'] = '1'
+            inf_1_filter = PointTypeMapFilter(inf_1_data)
+
+            inf_2_data = request.GET.copy()
+            inf_2_data['inference_version'] = '2'
+            inf_2_filter = PointTypeMapFilter(inf_2_data)
+
+            f1_ptm = inf_1_filter.qs
+            f2_ptm = inf_2_filter.qs
 
             bpt_filter = BrickPointTypeFilter(request.GET)
             bpt = bpt_filter.qs
@@ -36,10 +39,10 @@ class PointMappingView(CreateView):
             args = {
                 'bpt': bpt,
                 'hpt': hpt,
-                'ptm': ptm,
-                'ptm1': ptm1,
+                'ptm': f1_ptm,
+                'ptm1': f2_ptm,
                 'bpt_filter': bpt_filter,
                 'hpt_filter': hpt_filter,
-                'map_filter': map_filter
+                'map_filter': inf_1_filter
             }
             return render(request, self.template_name, args)
